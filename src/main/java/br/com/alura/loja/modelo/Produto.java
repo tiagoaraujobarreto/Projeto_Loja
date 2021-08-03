ebe27a85e3fc6626f7435f98a6724732baae6bde
tiagoaraujobarreto <tiagoaraujo.barreto@gmail.com>
2021-08-02 21:24:49 -0300

    Mapeamento de chaves compostas

--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -16,7 +16,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "produtos")
-@NamedQuery(name = "Produto.produtosPorCategoria", query = "SELECT p.preco FROM Produto p WHERE p.nome = :nome")
+@NamedQuery(name = "Produto.produtosPorCategoria", query = "SELECT p FROM Produto p WHERE p.categoria.id.nome = :nome")
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Produto {
 
